a1695890acdbab728c6a6afa22578c208d0ecf8c
HendrikClijstersMosa <hendrik.clijsters@mosa-rt.be>
2022-06-11 16:11:28 +0200

    Added particle storm exercise

---
+++ classes/Opdracht 1.14 Particle Storm/particle.py
@@ -0,0 +1,26 @@
+from random import random
+
+class Particle:
+    destroy_threshold = 0.999
+    # Init of particle class
+    def __init__(self, max_speed):
+        self.pos_x = 0
+        self.pos_y = 0
+        self.color = (255,255,255)
+
+        self.max_speed = max_speed
+        self.speed_x = self.max_speed*(2*random()-1)
+        self.speed_y = self.max_speed*(2*random()-1)
+
+    # Update the x and y position of a particle
+    def update(self,interval):
+        pass
+
+    # Checks if the paramters of a particle need to be reset.
+    # This can be done by calling the __init__ method of the particle.
+    def reset(self, interval):
+        pass
+
+    # Change the color of the particle depending on certain variables.
+    def set_color(self):
+        pass
